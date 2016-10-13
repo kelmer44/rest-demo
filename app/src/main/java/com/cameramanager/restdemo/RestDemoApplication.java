9ed09957020593e4d42c80a783d758b9b8437dad
Gabriel Sanmartin Diaz <gabriel.sanmartin.diaz@cameramanager.com>
2016-10-13 16:23:51 +0200

    MenuDrawer

---
+++ app/src/main/java/com/cameramanager/restdemo/RestDemoApplication.java
@@ -0,0 +1,10 @@
+package com.cameramanager.restdemo;
+
+import android.app.Application;
+
+/**
+ * Created by Gabriel Sanmartín on 10/13/2016.
+ */
+
+public class RestDemoApplication extends Application {
+}
