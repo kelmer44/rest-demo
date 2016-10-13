d30279c3f054496bc29c08de203396487dfac270
Gabriel Sanmartin Diaz <gabriel.sanmartin.diaz@cameramanager.com>
2016-10-13 16:02:50 +0200

    Proper package, starting truly from scratch

---
+++ app/src/main/java/com/cameramanager/restdemo/zones/ZonesActivity.java
@@ -0,0 +1,15 @@
+package com.cameramanager.restdemo.zones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cameramanager.restdemo.R;
+
+public class ZonesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
