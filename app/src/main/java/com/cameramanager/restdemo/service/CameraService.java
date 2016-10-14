1bb1360aad6eb1615a7f1dcdc574c6999cb40076
Gabriel Sanmartin Diaz <gabriel.sanmartin.diaz@cameramanager.com>
2016-10-14 16:14:52 +0200

    Basic CameraService

---
+++ app/src/main/java/com/cameramanager/restdemo/service/CameraService.java
@@ -0,0 +1,26 @@
+package com.cameramanager.restdemo.service;
+
+
+import com.cameramanager.restdemo.data.model.Camera;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+/**
+ * Created by Gabriel Sanmartín on 10/14/2016.
+ */
+
+public interface CameraService {
+
+
+    @GET("cameras")
+    public Observable<List<Camera>> getCameras();
+
+    @GET("cameras/{cameraId}")
+    public Observable<Camera> getCamera(@Path("cameraId") Long cameraId);
+
+    
+}
